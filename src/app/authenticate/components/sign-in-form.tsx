5a4b3dcc36890975d6105f211663e01dad3573bf
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-08-07 00:55:47 -0300

    Feat: sign-in

--- src/app/authenticate/components/sign-in-form.tsx
+++ src/app/authenticate/components/sign-in-form.tsx
@@ -23,6 +23,8 @@ import {
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   email: z.email("E-mail invalido"),
@@ -41,8 +43,37 @@ export default function SignInForm() {
     }
   });
 
+  const router = useRouter()
+
   async function onSubmit(values: FormValue){
-    //
+    const { data, error } = await authClient.signIn.email({
+        email: values.email,
+        password: values.password,
+        fetchOptions: {
+          onSuccess: () => {
+            router.push('/')
+          },
+          onError: (ctx) => {
+            if (ctx.error.code === "USER_NOT_FOUND") {
+              toast.error("E-mail não encontrado.");
+              return form.setError("email", {
+                message: "E-mail não encontrado.",
+              });
+            }
+            if (ctx.error.code === "INVALID_EMAIL_OR_PASSWORD") {
+              toast.error("E-mail ou senha inválidos.");
+              form.setError("password", {
+                message: "E-mail ou senha inválidos.",
+              });
+              return form.setError("email", {
+                message: "E-mail ou senha inválidos.",
+              });
+            }
+
+            toast.error(ctx.error.message);
+          }
+        }
+    });
   }
 
   return (
