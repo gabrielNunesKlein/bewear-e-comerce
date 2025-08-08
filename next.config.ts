4a8128a4cf5db82e25587b4831677766315b344a
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-08-08 00:11:25 -0300

    Feat: Products list and category list create home page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['fsc-projects-static.s3.us-east-1.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/components/common/product-list.tsx
@@ -0,0 +1,23 @@
+import { productTable, productVariantTable } from '@/db/schema';
+import React from 'react'
+import ProductItem from './product-item';
+
+interface ProductListProps {
+  title: string;
+  products: (typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  })[];
+}
+
+export default function ProductList({ products, title }: ProductListProps) {
+    return (
+        <div className='space-y-6'>
+            <h3 className='px-5 font-semibold'>{title}</h3>
+            <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+                {products.map((product) => (
+                    <ProductItem key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    )
+}
