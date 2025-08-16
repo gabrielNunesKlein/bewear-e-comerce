c67ce987e7c9bc3e6ba4dc00700392655c97345f
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-08-15 23:23:28 -0300

    Feat: route api webhook stripe success page and my-orders page

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,16 +1,11 @@
 'use client';
 import { createCheckoutSession } from '@/actions/create-checkout-session';
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogTitle } from '@/components/ui/dialog'
 import { useFinishOrder } from '@/hooks/mutatios/use-finish-order';
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
+import React from 'react'
 import { loadStripe } from "@stripe/stripe-js";
 
 export default function FinishOrderButton() {
-
-    const [openDialogSuccess, setOpenDialogSuccess] = useState(false)
 
     const finishOrder = useFinishOrder();
 
@@ -39,7 +34,6 @@ export default function FinishOrderButton() {
             sessionId: checkoutSession.id,
         });
 
-        setOpenDialogSuccess(true)
     }
 
 
@@ -47,39 +41,12 @@ export default function FinishOrderButton() {
         <>
             <Button
                 onClick={handleFinishOrder}
+                disabled={finishOrder.isPending}
                 size={'lg'}
                 className='rounded-full w-full'
             >
                 Finalizr Compra
             </Button>
-            <Dialog open={openDialogSuccess} onOpenChange={setOpenDialogSuccess}>
-                <DialogContent className='text-center'>
-                    <Image 
-                        src={'/illustration.svg'}
-                        width={300}
-                        height={300}
-                        className='mx-auto'
-                        alt='Sucess'
-                    />
-                    <DialogTitle className="mt-4 text-2xl">
-                        Pedido Efetuado!
-                    </DialogTitle>
-                    <DialogDescription className='font-medium'>
-                        Seu pedido foi efetuado com sucesso. Você pode acompanhar o status na seção de “Meus Pedidos”.
-                    </DialogDescription>
-
-                    <DialogFooter>
-                        <Button className="rounded-full" size="lg">
-                            Ver Compras
-                        </Button>
-                        <Button asChild size={'lg'} variant='outline' className='rounded-full'>
-                            <Link href={'/'}>
-                                Voltar Para Loja
-                            </Link>
-                        </Button>
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
         </>
     )
 }
